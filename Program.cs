8209e5b85f1b149a5c7368f76faa92ff37b38160
Krukov <da123@mail.ru>
2025-12-16 15:22:17 +0500

    Разработанный метод для получения ответов от GigaChat. Mamaev

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System.Net.Security;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using pr10.Models.Response;
 
 namespace pr10
 {
@@ -51,8 +53,48 @@ namespace pr10
 
             return ReturnToken;
         }
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
+            string Token = await GetToken(ClientId, AuthorizationKey);
+        }
+        public static async Task<ResponseMessage> GetAnswer(string token, string message)
+        {
+            ResponseMessage responseMessage = null;
+            string Url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions";
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+                using (HttpClient client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("Authorization", $"Bearer {token}");
+                    Models.Request DataRequest = new Models.Request()
+                    {
+                        model = "GigaChat",
+                        stream = false,
+                        repetition_penalty = 1,
+                        messages = new List<Models.Request.Message>()
+                        {
+                            new Models.Request.Message()
+                            {
+
+                                role="user",
+                                content=message
+                            }
+                        }
+                    };
+                    string JsonContent = JsonConvert.SerializeObject(DataRequest);
+                    Request.Content = new StringContent(JsonContent, Encoding.UTF8, "application/json");
+                    HttpResponseMessage Response = await client.SendAsync(Request);
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        responseMessage = JsonConvert.DeserializeObject<ResponseMessage>(ResponseContent);
+                    }
+                }
+            }
+            return responseMessage;
 
         }
     }
